890ef60674898f2e74b672929902ee40d6b21852
naokokobayashi <k1510059@edu.cc.uec.ac.jp>
2018-04-22 10:49:18 +0900

    体裁整え
    ファイル，フォルダ名を相対パスで指定できるよう変更

--- Zhang.py
+++ Zhang.py
@@ -5,10 +5,19 @@
 import numpy
 import cv2
 # get file name efficiently
-from glob import glob
 import os
 
 def main():
+
+    # 現在のスクリプトのフォルダの絶対パス取得
+    base = os.path.dirname(os.path.abspath(__file__)).replace("\\","/")
+    # チェスボード画像を格納するフォルダ
+    train_path = base + "/trainimages/"
+    # 歪み修正用画像，修正後の画像を保存するフォルダ
+    test_path = base + "/resultimages/"
+
+    # 歪み修正をする画像ファイル名
+    no_dist_img = "chess_16.jpg"
 
     square_size = 1.0      # 正方形のサイズ
     pattern_size = (10, 7)  # 模様のサイズ
@@ -17,40 +26,49 @@ def main():
     pattern_points *= square_size
     obj_points = []
     img_points = []
-    images = []
-    path = "D:/レポート(2018)/CG応用/images/"
 
-    for fn in os.listdir(path):
-        # 画像の取得
-        img = cv2.imread(fn, 0)
-        #cv2.imshow("test",img)
-        images.append(img)
+    for fn in os.listdir(train_path):
+        img = cv2.imread(train_path + fn, 0)
         print ("loading..." + fn)
-        cv2.imshow(img)
         # チェスボードのコーナーを検出
-        #found, corner = cv2.findChessboardCorners(img, pattern_size)
+        found, corner = cv2.findChessboardCorners(img, pattern_size)
         # コーナーがあれば
-        #if found:
-        #    term = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.1)
-        #    cv2.cornerSubPix(img, corner, (5,5), (-1,-1), term)
+        if found:
+            term = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.1)
+            cv2.cornerSubPix(img, corner, (5,5), (-1,-1), term)
         # コーナーがない場合のエラー処理
-        #if not found:
-        #    print ('chessboard not found')
-        #    continue
-        #img_points.append(corner.reshape(-1, 2))   #appendメソッド：リストの最後に因数のオブジェクトを追加
-        #obj_points.append(pattern_points)
+        if not found:
+            print ('chessboard not found')
+            continue
+        img_points.append(corner.reshape(-1, 2))   #appendメソッド：リストの最後に因数のオブジェクトを追加
+        obj_points.append(pattern_points)
         #corner.reshape(-1, 2) : 検出したコーナーの画像内座標値(x, y)
 
     # 内部パラメータを計算 rms,CameraMatrix,Distorition,rotate vec,translate vec
-    #rms, K, d, r, t = cv2.calibrateCamera(obj_points,img_points,(images[1].shape[1],images[1].shape[0]),None,None)
+    rms, K, d, r, t = cv2.calibrateCamera(obj_points,img_points,(img.shape[1],img.shape[0]),None,None)
     # 計算結果を表示
-#    print ("RMS = ", rms)
-#    print ("K = \n", K)
-#    print ("d = ", d.ravel())
+    print ("RMS = ", rms)
+    print ("CameraMatrix = \n", K)
+    print ("Distortion = ", d.ravel())
+    print ("rotate vec = \n", r)
+    print ("translate vec = \n", t)
     # 計算結果を保存
-#    numpy.savetxt("rms.csv", numpy.array([rms]), delimiter =',',fmt="%0.14f")
-#    numpy.savetxt("K.csv", K, delimiter =',',fmt="%0.14f")
-#    numpy.savetxt("Dist.csv", d, delimiter =',',fmt="%0.14f")
+    numpy.savetxt("rms.csv", numpy.array([rms]), delimiter =',',fmt="%0.14f")
+    numpy.savetxt("K.csv", K, delimiter =',',fmt="%0.14f")
+    numpy.savetxt("Dist.csv", d, delimiter =',',fmt="%0.14f")
+
+    # カメラパラメータの修正
+    img = cv2.imread(test_path+no_dist_img)
+    h,  w = img.shape[:2]
+    newcameramtx, roi=cv2.getOptimalNewCameraMatrix(K,d,(w,h),1,(w,h))
+
+    # undistort
+    dst = cv2.undistort(img, K, d, None, newcameramtx)
+
+    # crop the image
+    x,y,w,h = roi
+    dst = dst[y:y+h, x:x+w]
+    cv2.imwrite(test_path+'calibresult.jpg',dst)
 
 if __name__ == '__main__':
     main()
